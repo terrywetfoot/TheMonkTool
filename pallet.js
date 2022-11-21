45c2bb3a62df62719823c84c79f7a8abf9a1426a
terrywetfoot <abisset@posteo.net>
2022-11-21 09:14:50 +0000

    QOL +
    Added some QOL improvements to pallet tracker:
     - Warning displays if machine is set at impossible speed
     - User selection boxes make more sense now
     - Removed the date from each pallet time

--- pallet.js
+++ pallet.js
@@ -9,10 +9,9 @@ $(document).ready(function(){
     // Add the option for breaks 
     
 
- $("#no-of-pallets, #pallet-format, #machine-speed, #fillheads-on").on('change', function() { 
+ $("#no-of-pallets, #pallet-format, #machine-speed, #fillheads-on").on("change", function() { 
 
     const runningSpeed = $("#machine-speed").val()/60;
-
     const palletFormat = $("#pallet-format").val();
     const headsOn = $("#fillheads-on").val();
     const time = new Date();
@@ -24,24 +23,26 @@ $(document).ready(function(){
     const palletBuildTime = palletTimesAtMax / headsOn;
     const targetBuildTime = palletBuildTime.toFixed(0)
     
+
+    //Reminder to set the machine speed accordingly, if running at more than 5200 on 440ml cans. 
+    if (Number($("#machine-speed").val()) > 5200 && $("#pallet-format").val() === '1512'){
+      $("#warning").show();
+    } else if (Number($("#machine-speed").val()) > 5200 && $("#pallet-format").val() === '2160') {
+      $("#warning").show();
+    } else {
+      $("#warning").hide();
+    };
     
 
+    //Printing the pallet times 
     $("#pallet-time-list, #fastest-time").remove();
-    
-    $("#display-pallet-times").append(`<h4 id="fastest-time">Target time for each pallet: `+targetBuildTime+` minutes</h4><ol id="pallet-time-list"></ol>`);
-
+    $("#display-pallet-times").append(`<h4 id="fastest-time">Target time for each pallet: `+targetBuildTime+` minutes</h4>
+      <ol id="pallet-time-list"></ol>`);
     for (let i = 1; i < $("#no-of-pallets").val(); i++) { 
     timeOfNextPallet.setTime(timeOfNextPallet.getTime() + palletBuildTime * 60000);
-
-    let printTime = new Date(timeOfNextPallet).toLocaleDateString('en-GB', { 
-    hour:   '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    });
-
+    let printTime = new Date(timeOfNextPallet).toTimeString().substring(0, 5);
      $("#pallet-time-list").append(`<li>`+printTime+`</li>`)
     };
-    console.log("Time in 20 = " + timeOfNextPallet.getHours());
     });
 
 })
