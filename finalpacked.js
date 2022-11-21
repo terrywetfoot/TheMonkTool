90b8ca6c212172fb601b14781c6b25c516c90aec
terrywetfoot <abisset@posteo.net>
2022-11-21 12:39:58 +0000

    Replaced clunky Switch Statement
    Replaced the clunky Format Input switch statement for a much more concise and scaleable double for loop.

--- finalpacked.js
+++ finalpacked.js
@@ -7,33 +7,16 @@ $(document).ready(function(){
     //ADD BOX USAGE FOR 6x4
 
 // -- \\\\\\\\\\\\\\\\\\\\\ FINAL PACKED \\\\\\\\\\\\\\\\\\\\\\\   -- \\
-   $("#no-of-formats").on("change", function() { 
-    switch (Number($("#no-of-formats").val())) {
-        case 6:
-            $("#fm1, #fm2, #fm3, #fm4, #fm5, #fm6").show();
-            break;
-        case 5: 
-            $("#fm1, #fm2, #fm3, #fm4, #fm5").show();
-            $("#fm6").hide();
-            break;
-        case 4:
-            $("#fm1, #fm2, #fm3, #fm4").show();
-            $("#fm5, #fm6").hide();
-            break;
-        case 3:
-            $("#fm1, #fm2, #fm3").show();
-            $("#fm4, #fm5, #fm6").hide();
-            break;
-        case 2:
-            $("#fm1, #fm2").show();
-            $("#fm3, #fm4, #fm5, #fm6").hide();
-            break;
-        case 1:  
-            $("#fm1").show();
-            $("#fm2, #fm3, #fm4, #fm5, #fm6").hide();
-            break;
-    }
-   }); 
+    $("#no-of-formats").on("change", function() {
+        const userSelectedInputs = Number($("#no-of-formats").val());
+        const formatArray = [$("#fm1"), $("#fm2"), $("#fm3"), $("#fm4"), $("#fm5"), $("#fm6")];
+        for (let i = 0; i <= userSelectedInputs; i++) { 
+            formatArray[i].show();
+        };
+        for (let i = 5; i >= userSelectedInputs; i--) {
+            formatArray[i].hide();
+        };
+    });
 
    $("#format1, #format2, #format3, #format4, #format5, #format6").on("change", function() {
         //Stick those format options in an array for filtering
@@ -57,81 +40,81 @@ $(document).ready(function(){
 
 //--
    $("#calc-button").on("click", function( ) { 
-    //Case Formats Packed, the number of different formats that were packed in a packaging run.
-    const CFP = [
-        $("#format1").val(), 
-        $("#format2").val(), 
-        $("#format3").val(),
-        $("#format4").val(),
-        $("#format5").val(),
-        $("#format6").val()
-    ];
-    //Option chosen by user for each of the formats packed (CFP).    
-    const formatTypes = [
-        [`330__12 //:`,12],
-        [`330__24 //:`,24],
-        [`330_6*4 //:`,24],
-        [`440__12 //:`,12],
-        [`440__24 //:`,24],
-        [`440_6*4 //:`,24],
-        [``, 0]
-    ];
-    
-    //This function sets the user's input format type, to whichever they have chosen.    
-    function getCaseFormat(n){
-        switch (Number(n)) {
-            case 1:
-                return formatTypes[0][0];              
-            break;
-            case 2:
-                return formatTypes[1][0];               
-            break;
-            case 3:
-                return formatTypes[2][0];               
-            break;
-            case 5:
-                return formatTypes[3][0];         
-            break;
-            case 6:
-                return formatTypes[4][0];
-            break;
-            case 7:
-                return formatTypes[5][0];        
-            break;
-        default:
-                return formatTypes[6][0];        
-            break;
-    };
-        }
-    
-    //Running each format through the function     
-    let in1CaseFormat = getCaseFormat(CFP[0]);
-    let in2CaseFormat = getCaseFormat(CFP[1]);
-    let in3CaseFormat = getCaseFormat(CFP[2]);
-    let in4CaseFormat = getCaseFormat(CFP[3]);
-    let in5CaseFormat = getCaseFormat(CFP[4]);
-    let in6CaseFormat = getCaseFormat(CFP[5]);
-    
-    let in1 = Number($("#inputAmtCases1").val());
-    let in2 = Number($("#inputAmtCases2").val());
-    let in3 = Number($("#inputAmtCases3").val());
-    let in4 = Number($("#inputAmtCases4").val());
-    let in5 = Number($("#inputAmtCases5").val());
-    let in6 = Number($("#inputAmtCases6").val());
-//--
-    let inputLids = $("#input-lids").val()
-    let input330 = $("#input-330").val()
-    let input440 = $("#input-440").val()
-//--
-    let lidsUsed = Number( inputLids * 625 ).toLocaleString();
-    let used330 = Number( input330 * 351 ).toLocaleString();
-    let used440 = Number( input440 * 351 ).toLocaleString();
-//--
-    let lidsColour = $("#input-lids-colour").val().toUpperCase();
-    let canType330 = $("#input-330-can-type").val().toUpperCase();
-    let canType440 = $("#input-440-can-type").val().toUpperCase();
-//-- Brand & Gyle for display on the MAIN OUTPUT
+        //Case Formats Packed, the number of different formats that were packed in a packaging run.
+        const CFP = [
+            $("#format1").val(), 
+            $("#format2").val(), 
+            $("#format3").val(),
+            $("#format4").val(),
+            $("#format5").val(),
+            $("#format6").val()
+        ];
+        //Option chosen by user for each of the formats packed (CFP).    
+        const formatTypes = [
+            [`330__12 //:`,12],
+            [`330__24 //:`,24],
+            [`330_6*4 //:`,24],
+            [`440__12 //:`,12],
+            [`440__24 //:`,24],
+            [`440_6*4 //:`,24],
+            [``, 0]
+        ];
         
+        //This function sets the user's input format type, to whichever they have chosen.    
+        function getCaseFormat(n){
+            switch (Number(n)) {
+                case 1:
+                    return formatTypes[0][0];              
+                break;
+                case 2:
+                    return formatTypes[1][0];               
+                break;
+                case 3:
+                    return formatTypes[2][0];               
+                break;
+                case 5:
+                    return formatTypes[3][0];         
+                break;
+                case 6:
+                    return formatTypes[4][0];
+                break;
+                case 7:
+                    return formatTypes[5][0];        
+                break;
+            default:
+                    return formatTypes[6][0];        
+                break;
+        };
+            }
+        
+        //Running each format through the function     
+        let in1CaseFormat = getCaseFormat(CFP[0]);
+        let in2CaseFormat = getCaseFormat(CFP[1]);
+        let in3CaseFormat = getCaseFormat(CFP[2]);
+        let in4CaseFormat = getCaseFormat(CFP[3]);
+        let in5CaseFormat = getCaseFormat(CFP[4]);
+        let in6CaseFormat = getCaseFormat(CFP[5]);
+        
+        let in1 = Number($("#inputAmtCases1").val());
+        let in2 = Number($("#inputAmtCases2").val());
+        let in3 = Number($("#inputAmtCases3").val());
+        let in4 = Number($("#inputAmtCases4").val());
+        let in5 = Number($("#inputAmtCases5").val());
+        let in6 = Number($("#inputAmtCases6").val());
+    //--
+        let inputLids = $("#input-lids").val()
+        let input330 = $("#input-330").val()
+        let input440 = $("#input-440").val()
+    //--
+        let lidsUsed = Number( inputLids * 625 ).toLocaleString();
+        let used330 = Number( input330 * 351 ).toLocaleString();
+        let used440 = Number( input440 * 351 ).toLocaleString();
+    //--
+        let lidsColour = $("#input-lids-colour").val().toUpperCase();
+        let canType330 = $("#input-330-can-type").val().toUpperCase();
+        let canType440 = $("#input-440-can-type").val().toUpperCase();
+    //-- Brand & Gyle for display on the MAIN OUTPUT
+            
 //------THE MAIN OUTPUT -------------------------------------
 
 
@@ -146,6 +129,7 @@ $(document).ready(function(){
         const appendIfPacked = (amount, x) => {
             if (amount > 0) {return $(".nR").append(`${x} *${amount.toLocaleString()}* <br>`); }
         }; 
+
         appendIfPacked(in1,in1CaseFormat);
         appendIfPacked(in2,in2CaseFormat);
         appendIfPacked(in3,in3CaseFormat);
