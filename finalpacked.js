0374ced6b6e2f053f52b49d954e0f1209e1d88fc
terrywetfoot <abisset@posteo.net>
2022-11-30 14:50:26 +0000

    Removed much Chaff and admired some wheat.
    Removed unnecessary code, replacing declared variables with functions, utilising jQuery.
    
    Also changed .nr to #new-result as it is slightly more back-end user friendly.
    
    Removed a dash. from the console.log message at the very top because ADHD.

--- finalpacked.js
+++ finalpacked.js
@@ -2,11 +2,10 @@
 
 $(document).ready(function(){ 
 console.log("Welcome to TheMonkTool Console. This is where I debug!")
-console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 //TO DO
     //ADD WASTAGE 
     //ADD BOX USAGE FOR 6x4
-
 
 // -- \\\\\\\\\\\\\\\\\\\\\ FINAL PACKED \\\\\\\\\\\\\\\\\\\\\\\   -- \\
 
@@ -16,126 +15,70 @@ console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
             $(this).toggle(Number($("#no-of-formats").val()) >= index + 1);
         });    
     });    
-    // Check whether any formats selected are 330/440 (below or above value of 4 respectively)
+    // Check whether any formats selected are 330/440
     // then show the respective can inputs if they are
-    $(".format-select").on("change", function() {
-        let any330 = $(".format-select").filter(function(){
-            return $(".format-select").val() <= 3 }).length > 0;
-        let any440 = $(".format-select").filter(function(){
-            return $(".format-select").val() >= 5 }).length > 0;
-        $("#selected-330").toggle(any330);
-        $("#selected-440").toggle(any440);
+    $(".format-select, #no-of-formats").on("change", function() {
+        $("#selected-330").toggle($(".format-select:visible option:selected:contains(330)").length > 0);
+        $("#selected-440").toggle($(".format-select:visible option:selected:contains(440)").length > 0);
     });
 
-    
-//--
-   $("#calc-button").on("click", function( ) { 
-        //Case Formats Packed, the number of different formats that were packed in a packaging run.
-        const CFP = [
-            $("#format1").val(), 
-            $("#format2").val(), 
-            $("#format3").val(),
-            $("#format4").val(),
-            $("#format5").val(),
-            $("#format6").val()
-        ];
-        //Option chosen by user for each of the formats packed (CFP).    
-        const formatTypes = [
-            [`330__12 //:`,12],
-            [`330__24 //:`,24],
-            [`330_6*4 //:`,24],
-            [`440__12 //:`,12],
-            [`440__24 //:`,24],
-            [`440_6*4 //:`,24],
-            [``, 0]
-        ];
-        
-        //This function sets the user's input format type, to whichever they have chosen.    
-        function getCaseFormat(n){
-            switch (Number(n)) {
-                case 1:
-                    return formatTypes[0][0];              
-                break;
-                case 2:
-                    return formatTypes[1][0];               
-                break;
-                case 3:
-                    return formatTypes[2][0];               
-                break;
-                case 5:
-                    return formatTypes[3][0];         
-                break;
-                case 6:
-                    return formatTypes[4][0];
-                break;
-                case 7:
-                    return formatTypes[5][0];        
-                break;
-            default:
-                    return formatTypes[6][0];        
-                break;
-        };
-            }
-        
-        //Running each format through the function     
-        let in1CaseFormat = getCaseFormat(CFP[0]);
-        let in2CaseFormat = getCaseFormat(CFP[1]);
-        let in3CaseFormat = getCaseFormat(CFP[2]);
-        let in4CaseFormat = getCaseFormat(CFP[3]);
-        let in5CaseFormat = getCaseFormat(CFP[4]);
-        let in6CaseFormat = getCaseFormat(CFP[5]);
-        
-        let in1 = Number($("#inputAmtCases1").val());
-        let in2 = Number($("#inputAmtCases2").val());
-        let in3 = Number($("#inputAmtCases3").val());
-        let in4 = Number($("#inputAmtCases4").val());
-        let in5 = Number($("#inputAmtCases5").val());
-        let in6 = Number($("#inputAmtCases6").val());
-    //--
-        let inputLids = $("#input-lids").val()
-        let input330 = $("#input-330").val()
-        let input440 = $("#input-440").val()
-    //--
-        let lidsUsed = Number( inputLids * 625 ).toLocaleString();
-        let used330 = Number( input330 * 351 ).toLocaleString();
-        let used440 = Number( input440 * 351 ).toLocaleString();
-    //--
+    //Calculate function 
+    $("#calc-button").on("click", function( ) { 
+
+        let lidsUsed = Number( $("#input-lids").val() * 625 ).toLocaleString();
+        let used330 = Number( $("#input-330").val() * 351 ).toLocaleString();
+        let used440 = Number( $("#input-440").val() * 351 ).toLocaleString();
         let lidsColour = $("#input-lids-colour").val().toUpperCase();
         let canType330 = $("#input-330-can-type").val().toUpperCase();
         let canType440 = $("#input-440-can-type").val().toUpperCase();
-    //-- Brand & Gyle for display on the MAIN OUTPUT
-            
+
+        let printFormat = $(".format-select:visible option:selected").map(function() {
+            return $(this).text()
+            });
+        let printAmount = $(".inputAmtCases:visible").map(function() {
+            return $(this).val().toLocaleString() 
+            });
+
+        //TO DO: (For working out wastage)
+        // let total330CansPacked = (each 330 format 'amount' packed * its 'value' then summed) ;
+        // let total440CansPacked = (each 440 format 'amount' packed * its 'value' then summed) ;
+
 //------THE MAIN OUTPUT -------------------------------------
+     
+        $("#new-result").remove() /* Clears the slate */
+        $("#results").append(`<div id="new-result">
+            <p> ${$("#bg-input").val().toUpperCase()} </p>
+            PACKED__<br>`);
 
+        //Packed__
+        $(".fm:visible").each(function(i) {
+            if ( printAmount[i] > 0 ) {
+                return $("#new-result").append( `${printFormat[i]} //: *${printAmount[i]}*<br>` )
+                };
+            });
 
-
-        $(".nR").remove() /* Clears the slate */
-        $("#results").append(`<div class="nR">`); /*--Results Div Open--*/
-        
-/*GYLE*/$(".nR").append(`<p>`+ $("#bg-input").val().toUpperCase() + `</p>`);  
-        
-/*PAKD*/$(".nR").append(`PACKED__<br>`);
-
-        const appendIfPacked = (amount, x) => {
-            if (amount > 0) {return $(".nR").append(`${x} *${amount.toLocaleString()}* <br>`); }
+        //Cans__
+        $("#new-result").append(`<br>CANS__<br>`);   
+        if ( $("#input-330").val() > 0 ) {
+            $("#new-result").append(`330_${canType330} //: *${used330}*<br>`)
+        };
+        if ( $("#input-440").val() > 0 ) {
+            $("#new-result").append(`440_${canType440} //: *${used440}*<br>`)
         }; 
+        if ( used330 < 1 && used440 < 1 ) {
+            $("#new-result").append(`You forgot to add cans!<br>`)
+        };   
 
-        appendIfPacked(in1,in1CaseFormat);
-        appendIfPacked(in2,in2CaseFormat);
-        appendIfPacked(in3,in3CaseFormat);
-        appendIfPacked(in4,in4CaseFormat);
-        appendIfPacked(in5,in5CaseFormat);
-        appendIfPacked(in6,in6CaseFormat);
-    
-/*CANS*/$(".nR").append(`<br>CANS__<br>`);   
-        if (input330 > 0 ) {$(".nR").append(`330_${canType330} //: *${used330}*<br>`)};
-        if (input440 > 0 ) {$(".nR").append(`440_${canType440} //: *${used440}*<br>`)}; 
-        if (input330 < 1 && input440 < 1) {$(".nR").append(`You forgot to add cans!<br>`)};   
-/*LIDS*/$(".nR").append(`<br>LIDS__<br>`);    
-        if (inputLids > 0) {$(".nR").append(`${lidsColour} //: *${lidsUsed}*`)};
-        if (inputLids < 1) {$(".nR").append(`You forgot to add lids!<br>`)};    
-        $("#results").append(`</div>`); /*--Results Div Close--*/
+        //Lids__
+        $("#new-result").append(`<br>LIDS__<br>`);    
+        if ( $("#input-lids").val()  > 0 ) {
+            $("#new-result").append(`${lidsColour} //: *${lidsUsed}*`)
+        } else {
+            $("#new-result").append(`You forgot to add lids!<br>`)
+        };    
+
+        $("#results").append(`</p></div>`); /*--Results Div Close--*/
     
     
     }); //End of calculate function 
-});
+ });
