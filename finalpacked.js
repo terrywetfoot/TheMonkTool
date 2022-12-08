709c25b133508c0931718a3d6c3e8949fe59debf
terrywetfoot <abisset@posteo.net>
2022-12-08 15:26:41 +0000

    Added Fridge Packs to inputs and added Wastage
    Also added a half working Copy to Clipboard function. (Needs work on formatting)
    Also wrote a fair amount of code that needs refining.

--- finalpacked.js
+++ finalpacked.js
@@ -3,9 +3,6 @@
 $(document).ready(function(){ 
 console.log("Welcome to TheMonkTool Console. This is where I debug!")
 console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
-//TO DO
-    //ADD WASTAGE 
-    //ADD BOX USAGE FOR 6x4
 
 // -- \\\\\\\\\\\\\\\\\\\\\ FINAL PACKED \\\\\\\\\\\\\\\\\\\\\\\   -- \\
 
@@ -20,65 +17,159 @@ console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
     $(".format-select, #no-of-formats").on("change", function() {
         $("#selected-330").toggle($(".format-select:visible option:selected:contains(330)").length > 0);
         $("#selected-440").toggle($(".format-select:visible option:selected:contains(440)").length > 0);
+        $("#fridge-pack-330").toggle($(".format-select:visible option:selected:contains(330x6x4)").length > 0);
+        $("#fridge-pack-440").toggle($(".format-select:visible option:selected:contains(440x6x4)").length > 0);
     });
 
     //Calculate function 
     $("#calc-button").on("click", function( ) { 
-
-        let lidsUsed = Number( $("#input-lids").val() * 625 ).toLocaleString();
-        let used330 = Number( $("#input-330").val() * 351 ).toLocaleString();
-        let used440 = Number( $("#input-440").val() * 351 ).toLocaleString();
+        console.log('CALCULATING...')
+   
+        //Input variables
+        let lidsUsed = Number( $("#input-lids").val() * 625 );
+        let used330 = Number( $("#input-330").val() * 351 );
+        let used440 = Number( $("#input-440").val() * 351 );
         let lidsColour = $("#input-lids-colour").val().toUpperCase();
         let canType330 = $("#input-330-can-type").val().toUpperCase();
         let canType440 = $("#input-440-can-type").val().toUpperCase();
+        let fridgePacksUsed330 = Number( $("#fp-330").val() * $("#fp-330-per-box").val() );
+        let fridgePacksUsed440 = Number( $("#fp-440").val() * $("#fp-440-per-box").val() );
+        
 
-        let printFormat = $(".format-select:visible option:selected").map(function() {
-            return $(this).text()
-            });
-        let printAmount = $(".inputAmtCases:visible").map(function() {
-            return $(this).val().toLocaleString() 
-            });
 
-        //TO DO: (For working out wastage)
-        // let total330CansPacked = (each 330 format 'amount' packed * its 'value' then summed) ;
-        // let total440CansPacked = (each 440 format 'amount' packed * its 'value' then summed) ;
-
-//------THE MAIN OUTPUT -------------------------------------
-     
-        $("#new-result").remove() /* Clears the slate */
-        $("#results").append(`<div id="new-result">
-            <p> ${$("#bg-input").val().toUpperCase()} </p>
-            PACKED__<br>`);
-
-        //Packed__
+        //Wastage variables
+        let total330CansPacked = 0;
+        let total440CansPacked = 0;
         $(".fm:visible").each(function(i) {
-            if ( printAmount[i] > 0 ) {
-                return $("#new-result").append( `${printFormat[i]} //: *${printAmount[i]}*<br>` )
-                };
-            });
+            if ( $(this).find(".format-select:visible option:selected").text().includes("330") ) {
+            total330CansPacked += $(this).find(".inputAmtCases").val() * $(this).find(".format-select").val();
+            } else if ( $(this).find(".format-select:visible option:selected").text().includes("440") ) {
+            total440CansPacked += $(this).find(".inputAmtCases").val() * $(this).find(".format-select").val();            
+            }
+        });
+        let total330FridgePacksPacked = 0;
+        let total440FridgePacksPacked = 0;
+        $(".fm:visible").each(function(i) {
+            if ( $(this).find(".format-select:visible option:selected").text().includes("330x6x4") ) {
+            total330FridgePacksPacked += $(this).find(".inputAmtCases").val() * 6;
+            } else if ( $(this).find(".format-select:visible option:selected").text().includes("440x6x4") ) {
+            total440FridgePacksPacked += $(this).find(".inputAmtCases").val() * 6;
+            }
+        });
 
-        //Cans__
-        $("#new-result").append(`<br>CANS__<br>`);   
-        if ( $("#input-330").val() > 0 ) {
-            $("#new-result").append(`330_${canType330} //: *${used330}*<br>`)
+        //Wastage calculations
+        let wastage330 = used330 - total330CansPacked;
+        let wastage440 = used440 - total440CansPacked;
+        let wastageFridgePacks330 = fridgePacksUsed330 - total330FridgePacksPacked;
+        let wastageFridgePacks440 = fridgePacksUsed440 - total440FridgePacksPacked;
+        let wastageLids = lidsUsed - (total330CansPacked + total440CansPacked);
+
+        //Output variables
+        const newResult = 'new-result'; 
+        const $resultMessage = function(theMessage) {return $('<li>', {class: newResult, text: theMessage.toLocaleString() }); }
+        const $headingMessage = function(theMessage) {return $('<div>', {class: newResult, text: theMessage }); }
+        let $brandAndGyle = $resultMessage(`${$("#bg-input").val().toUpperCase()}`); 
+        let $can330Message = $resultMessage(`330_${canType330} //: *${used330}*`);
+        let $can440Message = $resultMessage(`440_${canType440} //: *${used440}*`);
+        let $lidsMessage = $resultMessage(`${lidsColour} //: *${lidsUsed}*`);
+        let $fridgePacks330Message = $resultMessage(`330_FP //: *${fridgePacksUsed330}*`);
+        let $fridgePacks440Message = $resultMessage(`440_FP //: *${fridgePacksUsed440}*`);
+        let $wastage330Message = $resultMessage(`330_W //: *${wastage330}*`);
+        let $wastage440Message = $resultMessage(`440_W //: *${wastage440}*`);
+        let $wastageLidsMessage = $resultMessage(`LIDS_W //: *${wastageLids}*`);
+        let $wastageFridgePacks330Message = $resultMessage(`330_FP //: *${wastageFridgePacks330}*`);
+        let $wastageFridgePacks440Message = $resultMessage(`440_FP //: *${wastageFridgePacks440}*`);
+
+
+        //Display functions
+        const  $display = function(x) {
+            return $("#print-box").append(x)
         };
-        if ( $("#input-440").val() > 0 ) {
-            $("#new-result").append(`440_${canType440} //: *${used440}*<br>`)
-        }; 
-        if ( used330 < 1 && used440 < 1 ) {
-            $("#new-result").append(`You forgot to add cans!<br>`)
-        };   
+        const  $checkThenDisplay = function(input, message) {
+            if (input >= 1) {
+                return $("#print-box").append(message)
+            }
+        };
+        const  $wastageDisplay = function(x) {
+            return $("#wastage-box").append(x)   
 
-        //Lids__
-        $("#new-result").append(`<br>LIDS__<br>`);    
-        if ( $("#input-lids").val()  > 0 ) {
-            $("#new-result").append(`${lidsColour} //: *${lidsUsed}*`)
+        };
+        const  $checkThenDisplayWastage = function(input, message) {
+            if (input >= 1) {
+                return $("#wastage-box").append(message)
+            }
+        };
+// //------THE MAIN OUTPUT -------------------------------------
+        
+        $('.new-result').remove(); //Clears the result if you click calculate again
+        $('.final-output').show(); //Shows the final output boxes
+
+        $display($brandAndGyle);
+        $display($headingMessage(`PACKED__`));
+        $(".fm:visible").each(function(i) {
+                    if ( $(this).find(".inputAmtCases:visible").val()  > 0 ) {
+                        $display($resultMessage(`${$(this).find(".format-select:visible option:selected").text()} //: 
+                            *${$(this).find(".inputAmtCases:visible").val()}*`))
+                        };
+                    });
+
+        //Cans            
+        $display($headingMessage(`CANS__`));
+        if (used330 < 1 && used440 < 1) {
+            $display($resultMessage(`You forgot to add cans!`));
+        }  else if (used330 >= 1 && used440 < 1) {
+            $display($can330Message);
+        } else if (used330 < 1 && used440 >= 1) {
+            $display($can440Message);
+        } else if (used330 >= 1 && used440 >= 1) {
+                $display($can330Message);
+                $display($can440Message);
+        };
+
+        //Lids
+        $display($headingMessage(`LIDS__`));
+        if (lidsUsed >= 1) {
+            $display($lidsMessage);  
         } else {
-            $("#new-result").append(`You forgot to add lids!<br>`)
-        };    
+            $display($resultMessage(`You forgot to add lids!`));
+        };
 
-        $("#results").append(`</p></div>`); /*--Results Div Close--*/
-    
-    
+        if (fridgePacksUsed330 >= 1 || fridgePacksUsed440 >= 1) {
+            $display($headingMessage(`FRIDGE PACKS__`));
+        };
+
+        //Fridge packs
+        $checkThenDisplay(fridgePacksUsed330, $fridgePacks330Message);
+        $checkThenDisplay(fridgePacksUsed440, $fridgePacks440Message);
+        
+        //Copy to clipboard
+        $("#copy-button").show();
+        $("#copy-button").on("click", function() {
+            console.log($("#print-box").text())
+            navigator.clipboard.writeText($("#print-box").text());
+            $("#copy-button").children().text("COPIED!");
+            setTimeout(() => {
+                $("#copy-button").children()
+                .text("COPY")
+            }, 1000);
+        });
+
+        //Wastage
+        $wastageDisplay($headingMessage(`WASTAGE__`));
+        $checkThenDisplayWastage(used330, $wastage330Message);
+        $checkThenDisplayWastage(used440, $wastage440Message);
+        $checkThenDisplayWastage(lidsUsed, $wastageLidsMessage);
+        $checkThenDisplayWastage(fridgePacksUsed330, $wastageFridgePacks330Message);
+        $checkThenDisplayWastage(fridgePacksUsed440, $wastageFridgePacks440Message);
+
+        //Wastage Warning
+        if (wastageLids >= 1200 || used330 >= 1 && wastage330 >= 550 || used440 >= 1 && wastage440 >= 550) {
+            $wastageDisplay($resultMessage(`This wastage looks too high. Maybe check your inputs?`));
+        };
+        if (wastageLids <= -1 || used330 >= 1 && wastage330 <= -1 || used440 >= 1 && wastage440 <= -1) {
+            $wastageDisplay($resultMessage(`There is negative wastage, or you've not added Cans/Lids. Maybe check your inputs?`));
+        };
+
     }); //End of calculate function 
  });
+        
